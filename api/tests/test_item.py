041470831399aefb9674519f780827fbfb5c9896
GaboGomez09 <gomezgabriel09@gmail.com>
2021-01-01 13:50:37 -0600

    BASIC TESTS WORK!!

--- api/tests/test_item.py
+++ api/tests/test_item.py
@@ -1,6 +1,12 @@
 import pytest
 from factories.client import ClientFactory
-from podemos.resources.client import save_client
+from podemos.resources.client import save_client, get_client, ClientModel
+from database import db_session
 
 def test_save_client():
-    assert True
+    save_client("a1", "Eduardo")
+    client = db_session.query(ClientModel).get("a1")
+    
+    assert client is not None
+
+    # Delete inserted row after
