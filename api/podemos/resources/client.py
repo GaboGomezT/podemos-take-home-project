6852f4af88d58dd136203e7cf82b5fea4d4d484c
GaboGomez09 <gomezgabriel09@gmail.com>
2021-01-01 13:39:39 -0600

    added get single client endpoint

--- api/podemos/resources/client.py
+++ api/podemos/resources/client.py
@@ -7,6 +7,13 @@ class ClientList(Resource):
         return {"clients": [client.json() for client in ClientModel.query.all()]}
 
 class Client(Resource):
+    def get(self):
+        _id = request.args.get("id", type=str)
+        if _id:
+            client = get_client(_id)
+            return client.json()
+        return {"message": "id cannot be empty"}
+
     def post(self):
         _id = request.args.get("id", type=str)
         nombre = request.args.get("nombre", type=str)
@@ -19,3 +26,6 @@ def save_client(_id, nombre):
     new_client = ClientModel(_id, nombre)
     db_session.add(new_client)
     db_session.commit()
+
+def get_client(_id):
+    return db_session.query(ClientModel).get(_id)
